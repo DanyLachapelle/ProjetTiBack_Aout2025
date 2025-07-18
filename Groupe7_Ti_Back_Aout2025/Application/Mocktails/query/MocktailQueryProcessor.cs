1139aa8ac9eb37d6627024eb38750013a308e820
DanyLachapelle <la228659@student.helha.be>
2025-07-18 02:03:53 +0200

    fix

---
+++ Groupe7_Ti_Back_Aout2025/Application/Mocktails/query/MocktailQueryProcessor.cs
@@ -0,0 +1,30 @@
+﻿using Application.DTOs;
+using Application.Mocktails.query.getAllMocktail;
+using Application.Mocktails.query.getbyidMocktail;
+using Application.Utils;
+
+namespace Application.Mocktails.query;
+
+public class MocktailQueryProcessor
+{
+    private readonly IQueryHandler<GetbyidMocktailQuery, MocktailDto> _getByIdMocktailHandler;
+    private readonly IQueryHandler<GetAllMocktailQuery, List<MocktailDto>> _getAllMocktailsHandler;
+    
+    public MocktailQueryProcessor(
+        IQueryHandler<GetbyidMocktailQuery, MocktailDto> getByIdMocktailHandler,
+        IQueryHandler<GetAllMocktailQuery, List<MocktailDto>> getAllMocktailsHandler)
+    {
+        _getByIdMocktailHandler = getByIdMocktailHandler;
+        _getAllMocktailsHandler = getAllMocktailsHandler;
+    }
+    
+    public MocktailDto GetMocktailById(GetbyidMocktailQuery query)
+    {
+        return _getByIdMocktailHandler.Handle(query);
+    }
+    
+    public List<MocktailDto> GetAllMocktails(GetAllMocktailQuery query)
+    {
+        return _getAllMocktailsHandler.Handle(query);
+    }
+}

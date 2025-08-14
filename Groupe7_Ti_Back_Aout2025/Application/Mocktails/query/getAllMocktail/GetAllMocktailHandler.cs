827c4b929211a6af403132aaeff9bacd6c6d18ad
DanyLachapelle <la228659@student.helha.be>
2025-08-15 00:32:21 +0200

    correction IsAvailable si warning

--- Groupe7_Ti_Back_Aout2025/Application/Mocktails/query/getAllMocktail/GetAllMocktailHandler.cs
+++ Groupe7_Ti_Back_Aout2025/Application/Mocktails/query/getAllMocktail/GetAllMocktailHandler.cs
@@ -47,10 +47,20 @@ public class GetAllMocktailHandler:IQueryHandler<GetAllMocktailQuery, List<Mockt
     {
         
         if (mocktail.MocktailIngredients.Any(mi =>
-                mi.Ingredient.Quantity == 0 || 
-                mi.Ingredient.Quantity <= mi.Ingredient.RestockThreshold))
+                mi.Ingredient.Quantity == 0 ))
         {
             return false;
+        }
+
+        foreach (var mi in mocktail.MocktailIngredients)
+        {
+            var stock = mi.Ingredient.Quantity;
+            var limit = mi.Ingredient.RestockThreshold;
+
+            // Calcul du statut
+            if (stock < limit * 1.5m) // critical
+                return false; // on bloque si un ingrédient est critique
+            // 'warning' et 'good' ne bloquent pas la disponibilité
         }
         
         // Si forceAvailable est explicitement false, le mocktail est forcé indisponible
